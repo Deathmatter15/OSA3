5ca84c6ec5feade3b771c05bcf4ace9086ea52cc
Deathmatter15 <hans_chu@outlook.com>
2024-10-28 10:13:19 +0000

    edit to assignment

--- Project/assignment3.py
+++ Project/assignment3.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+# Libraries 
+import sys 
+
+# main script
+def main():
+  if len(sys.argv) !=5:
+    print("Usage: ./assigment -l <port> -p <pattern>")
+    return
+
+  print(f"Script name: {sys.argv[0]}")
+  print(f"Port number: {sys.argv[2]}")
+  print(f"Pattern type: {sys.argv[4]}")
+
 #Python file for a multi-threaded network server in python.
 
 #Listening for network port commentions (>1024)
